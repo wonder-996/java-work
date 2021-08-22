c120c6810f22c73edc5b1f73bf372fe7fcfaa48f
wonder-996 <84025528+wonder-996@users.noreply.github.com>
2021-08-22 22:07:33 +0800

    Update UserMap.java

--- five week/findid/src/main/java/com/example/findid/Mapper/UserMap.java
+++ five week/findid/src/main/java/com/example/findid/Mapper/UserMap.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserMap {
 
-
+@Select("select id from students where id = #{id}")
   public   User getUserid(int id);
 }
